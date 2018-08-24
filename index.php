3f8c3608aaed8b16a575dc2e18e8906d43639ea7
QuentinDll <quentin.delplace13@gmail.com>
2018-08-24 09:51:38 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// declaration du tableaux
+$state = array(
+  '02' => 'Aisne',
+  '59' => 'Nord',
+  '60' => 'Oise',
+  '62' => 'Pas-de-Calais',
+  '80' => 'Somme'
+);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Exercice 5</title>
+    </head>
+    <body>
+    </body>
+</html>
